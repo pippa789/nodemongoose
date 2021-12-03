03ce77b2d23a4d6d4077854cc333ec981dcd7d22
allen <allenpip341@gmail.com>
2021-12-03 18:30:45 +0000

    just need the update now

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ require("./db/connection")
 
 const yargs = require("yargs");
 
-const { addMovie, listMovies } = require("./movie/movieMethods.js")
+const { addMovie, listMovies, deleteMovie } = require("./movie/movieMethods.js")
 
 const app = async (args) => {
     switch (process.argv[2]){
@@ -11,7 +11,10 @@ const app = async (args) => {
             break;
         case "list":
             listMovies();
-        break;
+            break;
+        case "del":
+            deleteMovie({title: args.title, actor: args.actor});
+            break;
         default:
             console.log("Incorrect command")
         break;
--- src/movie/movieMethods.js
+++ src/movie/movieMethods.js
@@ -1,19 +1,30 @@
+const { Mongoose, AggregationCursor } = require("mongoose");
 const Movie = require("./movieModels.js");
 
 exports.addMovie = async(movieObj) => {
     try{
         const movie = await new Movie(movieObj)
         await movie.save()
-        console.log(`added ${movie.title}`)
+        console.log(`added ${movie.title}`);
+        mongoose.connection.close();
     } catch (error){
         console.log(error)
     }
 }
-// exports.listMovies = async(movieObj) => {
-//     try{
-//         const allMovies = await Movie.find().toArray();
-//         console.log(allMovies)
-//     } catch(error){
-//         console.log(error)
-//     }
-// }
+exports.listMovies = async(movieObj) => {
+    try{
+        console.log(await Movie.find({}));
+        mongoose.connection.close();
+    } catch(error){
+        console.log(error)
+    }
+};
+exports.deleteMovie = async(movieObj) =>{
+    try{
+        console.log(await Movie.deleteOne({}));
+        console.log(`you have deleted ${movie.title}`);
+        mongoose.connection.close();
+    } catch (error){
+        console.log(error)
+    }
+}
